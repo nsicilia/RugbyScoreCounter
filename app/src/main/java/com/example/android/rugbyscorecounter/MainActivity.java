5f399c5898181a9eb6da40f759afc53bf388a478
nsicilia <nsicilia@oswego.edu>
2018-09-23 23:43:08 -0400

    Added functionallity to the try button

--- app/src/main/java/com/example/android/rugbyscorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/rugbyscorecounter/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.android.rugbyscorecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int HomeScore = 0;
+    int AwayScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ScoreForHomeTeam();
     }
+
+    public void ScoreForHomeTeam(){
+
+        /**
+         * This is the button for the Home Try
+         */
+        Button HomeTry = findViewById(R.id.HomeTry);
+        HomeTry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeScore += 5;
+
+                DisplayHomeScore(HomeScore);
+            }
+        });
+
+    }
+
+    public void DisplayHomeScore(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void DisplayAwayScore(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+
 }
